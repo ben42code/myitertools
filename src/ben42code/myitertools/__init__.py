9c80d4ef8d2db5ad024f56d59d3b0576d3332a8c
ben42code <1077747+ben42code@users.noreply.github.com>
2026-07-16 17:12:59 +0200

    refactor: privatize implementation modules behind package public API

--- src/ben42code/myitertools/__init__.py
+++ src/ben42code/myitertools/__init__.py
@@ -1,5 +1,5 @@
-from .islice_extended import islice_extended
-from .iteratorcounter import IteratorCounter
-from .streamsequence import StreamSequence
+from ._islice_extended import islice_extended
+from ._iteratorcounter import IteratorCounter
+from ._streamsequence import StreamSequence
 
 __all__ = ["islice_extended", "IteratorCounter", "StreamSequence"]
